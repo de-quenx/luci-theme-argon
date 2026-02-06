13389a9672d7ccd83ee490155b71f47d41d26f14
de quenx <fidzxidz@gmail.com>
2026-02-07 00:04:19 +0700

    update argon.lua

--- luasrc/model/cbi/argon.lua
+++ luasrc/model/cbi/argon.lua
@@ -12,14 +12,19 @@ mode.default = "normal"
 gaya_theme = s:option(ListValue, "gaya_theme", "Style Theme")
 gaya_theme:value("dark", "Dark")
 gaya_theme:value("glass", "Glass")
-gaya_theme.default = "dark"
+gaya_theme:value("blue", "Blue")
+gaya_theme:value("sdark", "SDark")
+gaya_theme:value("mint", "Mint")
+gaya_theme:value("gold", "Gold")
+gaya_theme:value("redx", "RedX")
+gaya_theme.default = "glass"
 
-gaya_theme.depends = {
-    mode = "dark"
-}
-
-primary = s:option(Value, "primary", "Primary Color")
+primary = s:option(Value, "primary", "Light/Normal Mode")
 primary.placeholder = "#3f5582"
 primary.default = "#3f5582"
+
+dark_primary = s:option(Value, "dark_primary", "Dark Mode")
+dark_primary.placeholder = "#3f5582"
+dark_primary.default = "#3f5582"
 
 return m
